203098663381f0e1f058ec6fcf00b9716e165b38
Torbjörn <torbjorn.rodstrom@gmail.com>
2024-04-22 21:59:45 +0200

    Add project files.

---
+++ Labb_2_Racing_F1/Car.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2_Racing_F1
+{
+    internal class Car
+    {
+        public static int RaceTrackDistance = 1000;
+        public static int time = 0;
+        public int Speed = 33;
+        public string Name { get; set; }
+        public int Distance = 0;
+        public bool finished = false;
+        //test
+        private readonly object lockObject = new object();
+
+        public Car (string name)
+        {
+            Name = name;
+            
+        }
+        //test
+        public bool IsFinished()
+        {
+            lock (lockObject)
+            {
+                return finished;
+            }
+        }
+        //test
+        public void SetFinished(bool value)
+        {
+            lock (lockObject)
+            {
+                finished = value;
+            }
+        }
+        public void Race()
+        {
+
+
+
+            bool Racing = true;
+
+                while (Racing)
+                {
+                    for (int i = 0; i < 31; i++)
+                    {
+                        if (i == 10)
+                        {
+                            Event(this);
+                            i = 0;
+                        }
+                        Thread.Sleep(1000);
+                        Distance = Distance + Speed;
+                        //Console.WriteLine($"{Name} has traveld {Distance}M ");
+
+                        if (!(Distance <= RaceTrackDistance))
+                        {
+                            Console.WriteLine($"{Name} Crosses the line!!!");
+
+                        //test
+                        SetFinished(true);
+
+                        //Finnished = true;
+
+                        Racing = false;
+                            i = 100;
+                        }
+                    }
+
+                }
+            Console.WriteLine("Exiting Race");
+
+
+        }
+        static int Event(Car car)
+        {
+            
+            Random random = new Random();
+            int randomNumber = random.Next(1, 51);
+
+            if (randomNumber == 1) // 1 in 50 chance (2%)
+            {
+                Console.WriteLine($"Fouleleak (2% Chance), {car.Name} Refueling... 30 Seconds");
+                Thread.Sleep(30000);
+                return 0;
+            }
+            else if (randomNumber <= 3) // 2 in 50 chance (4%)
+            {
+                Console.WriteLine($"Nails On The Track (4% Chance), {car.Name} Changing tires... 20 Seconds");
+                Thread.Sleep(20000);
+                return 0;
+            }
+            else if (randomNumber <= 8) // 5 in 50 chance (10%)
+            {
+                Console.WriteLine($"Tire wear (10% Chance), {car.Name} making pitstop... 10 Seconds");
+                Thread.Sleep(10000);
+                return 0;
+            }
+            else if (randomNumber <= 18) // 10 in 50 chance (20%)
+            {
+                Console.WriteLine($"Engine Wear (20% Chance), {car.Name} is slowing down");
+                return car.Speed -= 1;
+            }
+            else
+            {
+                Console.WriteLine($"Avoiding other driver, {car.Name} Breaks");
+                Thread.Sleep(500);
+                return 0;
+            }
+
+        }
+    }
+
+}
---
+++ Labb_2_Racing_F1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Labb_2_Racing_F1
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            Car car = new Car("ferrari");
+            Car car2 = new Car("RedBull");
+
+            Thread thread = new Thread(car.Race);
+            Thread thread2 = new Thread(car2.Race);
+            thread2.Start();
+            thread.Start();
+            //thread2.Join();
+            //thread.Join();
+            bool ferrari = car.IsFinished();
+            bool RedBull = car.IsFinished();
+
+            if (ferrari == true)
+            {
+                thread.Join();
+            }
+            else
+            {
+                thread2.Join();
+            }
+            
+            //while (!car.Finnished || !car2.Finnished)
+            //{
+            //    string input = Console.ReadLine();
+            //    Console.WriteLine($"{car.Name} Distance traveled: {car.Distance} / {Car.RaceTrackDistance} Speed: {car.Speed} m/s");
+            //    Console.WriteLine($"{car2.Name} Distance traveled: {car2.Distance} / {Car.RaceTrackDistance} Speed: {car2.Speed} m/s");
+            //}
+
+
+            //if (car.IsFinished() && !car2.IsFinished())
+            //{
+            //    Console.WriteLine($"GZ {car.Name}");
+            //    Console.WriteLine($"Sorry {car2.Name}");
+            //}
+            //if (!car.IsFinished() && car2.IsFinished())
+            //{
+            //    Console.WriteLine($"GZ {car2.Name}");
+            //    Console.WriteLine($"Sorry {car.Name}");
+            //}
+            //if (car.IsFinished() && car2.IsFinished())
+            //{
+            //    Console.WriteLine("Hej");
+            //}
+            //if (car.IsFinished() && !car2.IsFinished())
+            //{
+            //    Console.WriteLine($"GZ {car.Name}");
+            //    Console.WriteLine($"Sorry {car2.Name}");
+            //}
+            //else if (!car.IsFinished() && car2.IsFinished())
+            //{
+            //    Console.WriteLine($"GZ {car2.Name}");
+            //    Console.WriteLine($"Sorry {car.Name}");
+            //}
+            //else if (car.IsFinished() && car2.IsFinished())
+            //{
+            //    Console.WriteLine("Hej");
+            //}
+            if (ferrari && !RedBull)
+            {
+                Console.WriteLine($"Congratulations {car.Name}!");
+                Console.WriteLine($"Sorry {car2.Name}");
+            }
+            else if (!ferrari && RedBull)
+            {
+                Console.WriteLine($"Congratulations {car2.Name}!");
+                Console.WriteLine($"Sorry {car.Name}");
+            }
+            else if (ferrari && RedBull)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+        }
+    }
+}
